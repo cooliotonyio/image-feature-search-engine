3206e29ed2eb627eeec89b32cb48db7f9ac6a18c
cooliotonyio <tony.ytzhao@gmail.com>
2018-12-03 13:56:02 -0800

    you've got to be kidding me

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def upload_file():
     elif 'filename' in request.args:
         full_filename = os.path.join(app.config['UPLOAD_FOLDER'], request.args['filename'])
         distances, paths = search_engine.query(full_filename)
-        html_imgs = ["<img src={}>".format(path) for path in paths]
+        html_imgs = ["<img src='{}'>".format(path) for path in paths]
         html = '''
             <!doctype html>
             <title>Results</title>
